a95fcd8c13d590946c41ee33ab03bcedb6a12e52
TanmayIndia <109366586+TanmayIndia@users.noreply.github.com>
2023-09-22 12:50:03 +0530

    File12_4.c

--- File12_4.c
+++ File12_4.c
@@ -1,41 +1,50 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/mman.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+	#include<stdio.h>
+	#include<unistd.h>
+	#include<sys/mman.h>
+	#include<sys/types.h>
+	#include<sys/stat.h>
+	#include<fcntl.h>
 
-#define PAGE 1024
+	int main(int argc, char * argv[])
+	{
+	int Ret=0,Fd=0,iCnt=0;
+	char *map=NULL;
+	struct stat Fobj;
 
-int main()
-{
-int Fd=0,i=0;
-char *map=NULL;
-struct stat sb;
-int FileSize=0;
+	if(argc<2)
+	{
+	printf("Please enter sufficient arguments\n");
+	return -1;
+	}
 
-Fd=open("new.txt",O_RDONLY);
+	Fd=open(argv[1],O_RDONLY);
 
-if(Fd==-1)
-{
-printf("Unable to open file.\n");
-return -1;
-}
+	if(Fd==-1)
+	{
+	printf("Not able to open file: %s",argv[1]);
+	return -1;
+	}
 
-fstat(Fd,&sb);
+	fstat(Fd,&Fobj);
 
-FileSize= sb.st_size;
+	if(!S_ISREG(Fobj.st_mode))
+	{
+	printf("This is not a regular file.\n");
+	return -1;
+	}
 
-map= mmap(NULL,FileSize,PROT_READ,MAP_PRIVATE,Fd,0);
+	map=mmap(0,Fobj.st_size,PROT_READ,MAP_SHARED,Fd,0);
 
-printf("Data from file is:\n");
-printf("========================================================\n");
-for(i=0;i<=FileSize;i++)
-{
-printf("%c",map[i]);
-}
-printf("=========================================================\n");
-return 0;
-}
+	for(iCnt=0;iCnt<Fobj.st_size;iCnt++)
+	{
+	printf("%c",map[iCnt]);
+	}
+	
+	if(munmap(map,Fobj.st_size)==-1)
+	{
+	printf("Error while un-mapping memory.\n");
+	return -1;
+	}
 
+	return 0;
+	}
